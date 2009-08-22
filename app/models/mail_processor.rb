27a64228d8a11a3613d45290c2637fcc02d46fbd
Chap Ambrose <chapambrose@gmail.com>
2009-08-21 23:51:15 -0400

    last commit before switching to attachment_fu '

--- app/models/mail_processor.rb
+++ app/models/mail_processor.rb
@@ -5,17 +5,20 @@ class MailProcessor < ActionMailer::Base
 	def receive(mail)
 		puts "Receiving a message with the subject '#{mail.subject}'"
 		
-		#photo = Photo.new
 		mms = MMS2R::Media.new(mail)
-		mms.media['image/jpeg'].each do |file|
+		images = []
+		images += mms.media['image/jpeg'] unless mms.media['image/jpeg'].nil?
+		images += mms.media['image/jpg'] unless mms.media['image/jpg'].nil?
+		images.each do |image|
 			begin
-				photo = Photo.create(:name => mms.subject, :description => mms.body, :uploaded_data => file)
+				photo = Photo.create(:name => mms.subject, :description => mms.body, :image => image)
 				puts 'photo created'
 			rescue => e
 				puts e
 			end
 		end
 		mms.purge
+		return nil
 	end
 	
 	def self.test
