2fdb0b6eb55da06cd35e6f3be624f2398932af42
Chap Ambrose <chapambrose@gmail.com>
2009-08-23 12:13:42 -0400

    still working on processor

--- app/models/mail_processor.rb
+++ app/models/mail_processor.rb
@@ -2,13 +2,27 @@ class MailProcessor < ActionMailer::Base
 	
 	def receive(mail)
 		from = mail.from_addrs.first.address
-		puts "Receiving a message with the subject '#{mail.subject}' from '#{from}'"
+		logger.info "Receiving a message with the subject '#{mail.subject}' from '#{from}'"
 		
 		user = User.find_by_full_email(from)
 		unless user
-			user = User.create(:full_email => mail.from_addrs.first.address,
+			user = User.create(:full_email => from,
 												 :email_name => MailProcessor.email_name(from))
 			puts 'sending welcome email'
+			Notifier.deliver_welcome(user)
+		end
+	end
+	
+	def self.test
+		mail = TMail::Mail.load("public/test.mail")
+		from = mail.from_addrs.first.address
+		logger.info "Receiving a message with the subject '#{mail.subject}' from '#{from}'"
+		
+		user = User.find_by_full_email(from)
+		unless user
+			user = User.create(:full_email => from,
+												 :email_name => MailProcessor.email_name(from))
+			logger.info 'sending welcome email'
 			Notifier.deliver_welcome(user)
 		end
 		
@@ -32,51 +46,7 @@ class MailProcessor < ActionMailer::Base
 				photo.save
 				
 				if exisiting_photo
-					exisiting_photo.destroy
-					Notifier.deliver_duplicate(user, photo)
-				end
-			rescue => e
-				puts e
-			end
-		end
-		mms.purge
-	end
-	
-	def self.test
-		mail = TMail::Mail.load("/var/www/photobagel.com/current/public/test.mail")
-		from = mail.from_addrs.first.address
-		puts "Receiving a message with the subject '#{mail.subject}' from '#{from}'"
-		
-		user = User.find_by_full_email(from)
-		unless user
-			user = User.create(:full_email => mail.from_addrs.first.address,
-												 :email_name => MailProcessor.email_name(from))
-			puts 'Creating new user. Sending welcome email'
-			Notifier.deliver_welcome(user)
-		end
-		
-		mms = MMS2R::Media.new(mail, :logger => puts)
-		images = []
-		images += mms.media['image/jpeg'] unless mms.media['image/jpeg'].nil?
-		images += mms.media['image/jpg'] unless mms.media['image/jpg'].nil?
-		
-		images.each do |image|
-			begin
-				image_info = EXIFR::JPEG.new(image)
-				exisiting_photo = Photo.find_by_taken_at(MailProcessor.taken_at(image_info))
-				
-				photo = user.photos.new(:name => mms.subject,
-														 :description => mms.body,
-														 :image => File.new(image))
-				photo.taken_at = MailProcessor.taken_at(image_info)
-				photo.camera_model = image_info.model if image_info.model
-				photo.latitude = MailProcessor.dms_degrees(image_info.exif.gps_latitude, image_info.exif.gps_latitude_ref) if image_info.exif.gps_latitude
-				photo.longitude = MailProcessor.dms_degrees(image_info.exif.gps_longitude, image_info.exif.gps_longitude_ref) if image_info.exif.gps_longitude
-				photo.save
-				
-				if exisiting_photo
-					puts 'Exisiting photo found.'
-					puts 'Deleting old photo and sending warning email.'
+					logger.info 'Duplicate photo found. Deleting and sending warning email.'
 					exisiting_photo.destroy
 					Notifier.deliver_duplicate(user, photo)
 				end
