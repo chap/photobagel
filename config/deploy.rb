de130cbde3fc03273fba386bff721a22bc3f08d1
Chap Ambrose <chapambrose@gmail.com>
2009-08-23 02:01:51 -0400

    bump

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,7 +5,7 @@ set :repository,  "git@github.com:railsrumble/rr09-team-226.git"
 set :scm, "git"
 set :scm_passphrase, "chug0misact"
 set :user, "root"
-set :runner, user
+set :runner, "root"
 set :branch, "master"
 
 set :deploy_to, "/var/www/#{application}"
@@ -14,26 +14,36 @@ role :app, "69.164.192.66"
 role :web, "69.164.192.66"
 role :db,  "69.164.192.66", :primary => true
 
-namespace :photobagel do
- 
-  desc "Starts mail fetcher"
-	task :start, :roles => :app do
-		puts 'Starting the mail fetcher'
-		sudo "RAILS_ENV=production #{current_path}/script/mail_fetcher start"
-	end
-	
-	desc "Stops mail fetcher"
-	task :stop, :roles => :app do
-		puts 'Stopping the mail fetcher'
-		sudo "RAILS_ENV=production #{current_path}/script/mail_fetcher stop"
-	end
-	
-	desc "Restarting mod_rails with restart.txt"
+namespace :deploy do
+  desc "Restarting mod_rails with restart.txt"
   task :restart, :roles => :app, :except => { :no_release => true } do
     run "touch #{current_path}/tmp/restart.txt"
-		photobagel.stop
-		photobagel.start
+  end
+ 
+  [:start, :stop].each do |t|
+    desc "#{t} task is a no-op with mod_rails"
+    task t, :roles => :app do ; end
+  end
+end
+
+namespace :mail_fetcher do
+  desc "Start mail_fetcher process" 
+  task :start, :roles => :app do
+    run "cd #{current_path}; #{rails_env} script/mail_fetcher start" 
   end
 
-	after "deploy:restart" , "photobagel:restart"
+  desc "Stop mail_fetcher process" 
+  task :stop, :roles => :app do
+    run "cd #{current_path}; #{rails_env} script/mail_fetcher stop" 
+  end
+
+  desc "Restart delayed_job process" 
+  task :restart, :roles => :app do
+    mail_fetcher.stop
+		mail_fetcher.start
+  end
 end
+
+after "deploy:start", "mail_fetcher:start" 
+after "deploy:stop", "mail_fetcher:stop" 
+after "deploy:restart", "mail_fetcher:restart"
